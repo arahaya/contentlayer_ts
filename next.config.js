ddfba46197df47d1475a8c060a9a851612f0aedc
ARAKI Hayato <arahaya.com@gmail.com>
2023-01-23 13:46:45 +0900

    scss with glob

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 const { withContentlayer } = require("next-contentlayer");
 
+const path = require('path');
+const globImporter = require('node-sass-glob-importer');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: { importer: globImporter(), includePaths: [path.join(__dirname, './src/styles/globals.scss')], },
 }
 
 module.exports = withContentlayer(nextConfig)
